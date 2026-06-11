6e335146f676d9a94eaa0bb208783f830462bd00
Blu3-Sky-Max <thebigshadow001@gmail.com>
2026-06-11 15:27:31 -0700

    feat(dsk-daemon.sh):add scaffold of dskd

---
+++ dsk-daemon.sh
@@ -0,0 +1,27 @@
+#!/bin/bash 
+
+#  =================================    Disk Daemon Management      ========================================================
+# shoutout to tlp for me using there steps for this deamon setups 
+#
+#
+# this deamon helps to manage mounted dir
+# lvm
+# autofs
+# nfs 
+# dev 
+
+# ===========================================================================================================================
+
+
+
+# deamon running every sec 
+while true; do 
+       
+
+
+
+done 
+
+
+
+    
