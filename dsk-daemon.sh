39523da89a2158016a9089a2cb35d957cc93d124
Blu3-Sky-Max <thebigshadow001@gmail.com>
2026-07-05 14:58:27 -0700

    fix: add umount on threshold breach, harden checks, prevent silent failures

--- dsk-daemon.sh
+++ dsk-daemon.sh
@@ -21,13 +21,13 @@
 # ===========================================================================================================
 
 # locator for config
-#
+
 Locator="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 CONF_FILE="$Locator/dsk-daemon.conf"
 
 if [ ! -f "$CONF_FILE" ]; then 
 	    echo "Configuration file not found: $CONF_FILE"   
-	exit 1
+	exit 20
 
   fi 
 
@@ -40,12 +40,11 @@ rotated=
 
 
 
-# Verify if the path is Directory or not with red color for error info
-# for color modification you can use check willam shotts textbook  
+# Verify if the path is Directory or not 
 
 if [[ ! -d "$WATCH_PATH" ]]; then
-    echo -e  "\033[31m[ERROR] WATCH_PATH does not exist or is not a directory: $WATCH_PATH \033[0m" >&2
-    exit 1
+    echo -e  "\n [ERROR]WATCH_PATH does not exist or is not a directory: $WATCH_PATH " >> "$LOG_FILE"  
+    exit 21
 fi
 
 
@@ -66,10 +65,10 @@ get_fs_info(){
 }
 
 # unknown user input control 
-#
 show_usage() {
     echo "Usage: $0 {start|stop|status|run|restart}" 1>&2
 }
+
 # Returns total human-readable size of WATCH_PATH 
 
 get_dir_size() {
@@ -81,22 +80,27 @@ get_dir_size() {
 # Rotate log if it has grown beyond MAX_LOG_LINES 
 
 rotate_log_if_needed() {
+    local lines
+
     if [[ -f "$LOG_FILE" ]]; then
- # local variable 
-        local lines
+
         lines=$(wc -l < "$LOG_FILE")
 
+
 # lines is beyond default size 250  the it rotate 
-#
+
         if (( lines > MAX_LOG_LINES )); then
 
-# handle the log_file. date with hr and sec 
-             rotated="${LOG_FILE}.$(date +%Y%m%d-%I%M%S)"
+# handle the log_file. date with hrs and sec 
+
+             rotated="${LOG_FILE}-$(date +%Y%m%d-%I:%S)"
+
+
 
 # then we mv full log file to log.date we going to have (dsk.log.date) 
             mv "$LOG_FILE" "$rotated"
 
-            echo "[$(date '+%Y-%m-%d %I:%M:%S')] Log rotated → $rotated" >> "$LOG_FILE"
+            echo "[$(date '+%Y-%m-%d %I:%M:%S')] Log rotated -->  $rotated" >> "$LOG_FILE"
         fi
     fi
 }
@@ -105,26 +109,28 @@ rotate_log_if_needed() {
 core_machine_loop(){ 
 
  
+echo -e  "Daemon Started  [$(date '+%Y-%m-%d %I:%M:%S')]. Watching: "$WATCH_PATH" Info: $(get_fs_info)" 
 
-# Colored output to terminal only
+
+# Output for logfile 
  
-echo -e  "\033[36mDaemon Started \033[0m [$(date '+%Y-%m-%d %I:%M:%S')].\033[36mWatching:\033[0m "$WATCH_PATH" \033[36m Info: \033[0m $(get_fs_info)" 
-
-# Clean output to log file only
-echo "Daemon Started [ $(date '+%Y-%m-%d %I:%M:%S')]. Watching: $WATCH_PATH Info: $(get_fs_info)" >> "$LOG_FILE"
+echo -e  "\nDaemon Started [$(date '+%Y-%m-%d %I:%M:%S')]. Watchings: $WATCH_PATH Info: $(get_fs_info) " >> "$LOG_FILE"
 
 
-#daemon running every sec
+# Daemon running 
 
  while true; do
       local Set_timestamp dir_size fs_used_pct  path_all_size msg plain_msg 
        Set_timestamp=$(date '+%Y-%m-%d %I:%M:%S') 
-      dir_size=$(get_dir_size)
-      fs_used_pct=$(get_fs_usage_percent)
+      
+     dir_size=$(get_dir_size)
+ 
+       fs_used_pct=$(get_fs_usage_percent)
+ 
    	 path_all_size=$(get_fs_info | awk '{print $3}')
 
 {    
-		echo "=====================================================" 
+		echo "===============================================================" 
 		echo "  Timestamp : $Set_timestamp"
 		echo "  Path      : $WATCH_PATH" 
 		echo "  Dir size  : $dir_size"
@@ -133,37 +139,66 @@ echo "Daemon Started [ $(date '+%Y-%m-%d %I:%M:%S')]. Watching: $WATCH_PATH Info
 
         } >> "$LOG_FILE"
 
+            
+# break if the path i not mounted or just a dir
+ if ! mountpoint -q "$WATCH_PATH"; then
+            echo "(ERROR)[$Set_timestamp] $WATCH_PATH is no longer mounted or is just a dir —->  daemon stopping" >> "$LOG_FILE"
+            logger -i "(ERROR)[$Set_timestamp] $WATCH_PATH is no longer mounted or just a dir — daemon stopping"
+            exit 33
+        fi
+
+
 # thresold trigger setting i.e if the percentage grows beyond the warn percent it triggers 
 if (( fs_used_pct >= WARN_PERCENT )); then
 
-# message to show on terminal with color 
-             msg="\033[31m(Warning)\033[0m[$Set_timestamp] Filesystem at ${fs_used_pct}% — threshold is ${WARN_PERCENT}%"
-		
-# messsage to show without color 
-	plain_msg="(Warning)[$Set_timestamp] Filesystem at ${fs_used_pct}% — threshold is ${WARN_PERCENT}%"
+breaks="(Warning)[$Set_timestamp] Filesystem at ${fs_used_pct}% — threshold IS SAME AS ${WARN_PERCENT}% and unmounted"
 
-# broadcast message to  every logged-in terminal. check "man wall "
-            echo "$msg" | wall  
-
- # message to send to log file 
-    echo "$plain_msg" >>  "$LOG_FILE" 
-
-
-# logging to system message journalctl 
-		logger -i "$plain_msg"
-# want the message sent via email 
+plain_msg="(Warning)[$Set_timestamp] Filesystem at ${fs_used_pct}% — threshold is Beyond ${WARN_PERCENT}%"
+     
+  if (( "$fs_used_pct" == "$WARN_PERCENT" )) ; then 
  
-#		mail -s "Disk Warning" email@gmail.com 
+   if umount "$WATCH_PATH"; then
+
+      echo "$breaks" >>  "$LOG_FILE" 
+      logger -i "$breaks"  
+             exit 31
+else
+      echo "(ERROR)[$Set_timestamp] umount failed for $WATCH_PATH" >> "$LOG_FILE" 
+          sleep "$Dang"
+             continue
+
+fi 
+# mail -s "Disk Warning"  email@gmail.com 
+        
+elif 
+        (( "$fs_used_pct" >  "$WARN_PERCENT" )); then 
+ 
+          if umount "$WATCH_PATH"; then 
+          echo "$plain_msg" >>  "$LOG_FILE" 
+		logger -i "$plain_msg"
+ exit 32 
+else
+                    echo "(ERROR)[$Set_timestamp] umount failed for $WATCH_PATH" >> "$LOG_FILE"
+                 sleep "$Dang"
+                    continue
+
+                fi
+
+# enable message to be sent to email  
+# mail -s "Disk Warning" email@gmail.com 
 
         fi
+fi  
    sleep "$Dang" 
-
+#check log file in every iteration 
+rotate_log_if_needed
 done
 
 
 
 }
-# ========================= LifeCycle =================================================
+
+# LifeCycle ============
 
 kill_pid= 
 
@@ -175,12 +210,14 @@ PID_FILE="/tmp/dsk-daemon.pid"
 cmd="${1:-run}"
 
 
-case $cmd in 
+case $cmd in
+ 
      Start|start|START)
+
         # the PID doesn't exit here so nothing to kill 
        if [[ -f "$PID_FILE" ]] && kill -0 "$(cat "$PID_FILE")" 2>/dev/null; then
             echo "[INFO] Daemon already running (PID $(cat "$PID_FILE"))"
-            exit 0
+            exit 22
         fi
          # let the loop run in background 
         core_machine_loop  &
@@ -205,6 +242,7 @@ case $cmd in
             echo -e " \033[31m(INFO) No PID file found — daemon may not be running \033[0m"
         fi
         ;;
+
       #  Status 
         status|Status|STATUS)
         if [[ -f "$PID_FILE" ]] && kill -0 "$(cat "$PID_FILE")" 2>/dev/null; then
@@ -213,18 +251,15 @@ case $cmd in
             echo -e  "\033[31m(INFO) Daemon is not running\033[0m "
         fi
         ;;
+
     run|Run|RUN)
        # pid already exit and it running just like restart
         core_machine_loop
         ;;
     
-     restart|RESTART|Restart) 
- # SAME AS RUN 
-         core_machine_loop
-            ;; 
      *) # for unknown input 
        show_usage 
-        exit 1 
+        exit 23
          ;; 
 
 
@@ -236,5 +271,5 @@ case $cmd in
 
 
 
-# turning debugging off. can also use shell check 
+
 #set +x
